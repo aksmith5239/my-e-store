b0a1e03e8e886d0f8633e7e00ad5e9202d776271
aksmith5239 <aklobby@gmail.com>
2020-10-24 12:09:49 -0600

    started category route logic

--- models/index.js
+++ models/index.js
@@ -8,22 +8,22 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'product_id'
 });
-// Categories have many Products
-// Category.hasMany(Product, {
-//   foreignKey: 'category_id'
-// });
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-   through: ProductTag, 
-   as: 'tagged_products',
-   foreignKey: 'product_id'
+//Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
 });
+// Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//    through: ProductTag, 
+//    as: 'tagged_products',
+//    foreignKey: 'product_id'
+// });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag,
-  as: 'tagged_products',
-  foreignKey: 'tag_id'
-})
+// Tag.belongsToMany(Product, {
+//   through: ProductTag,
+//   as: 'tagged_products',
+//   foreignKey: 'tag_id'
+// })
 
 
 module.exports = {
