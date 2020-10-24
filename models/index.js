37d765c227213a156d0f24e6db57dec21acc7b78
aksmith5239 <aklobby@gmail.com>
2020-10-24 13:12:41 -0600

    basic tag routes created

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
-// const Tag = require('./Tag');
-// const ProductTag = require('./ProductTag');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -12,23 +12,23 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
-// Products belongToMany Tags (through ProductTag)
-// Product.belongsToMany(Tag, {
-//    through: ProductTag, 
-//    as: 'tagged_products',
-//    foreignKey: 'product_id'
-// });
-// Tags belongToMany Products (through ProductTag)
-// Tag.belongsToMany(Product, {
-//   through: ProductTag,
-//   as: 'tagged_products',
-//   foreignKey: 'tag_id'
-// })
+//Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+   through: ProductTag, 
+   as: 'tagged_products',
+   foreignKey: 'product_id'
+});
+//Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'tag_id'
+})
 
 
 module.exports = {
   Product,
-  Category
-  // Tag,
-  // ProductTag,
+  Category,
+  Tag,
+  ProductTag,
 };
